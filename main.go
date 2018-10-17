a46f2be69bd3ad00d68221182b871c9b58c309ab
moratorium08 <moratorium08@gmail.com>
2018-10-17 15:08:36 +0900

    Implement uploading functions

--- main.go
+++ main.go
@@ -9,11 +9,17 @@ import (
 
 	"github.com/BurntSushi/toml"
 	flags "github.com/jessevdk/go-flags"
+	"github.com/nlopes/slack"
 )
 
 var opts struct {
-	Config string `short:"c" long:"config" description:"load config file(toml)" value-name:"CONFIG"`
-	File   string `positional-args:"yes" required:"yes"`
+	Config  string `long:"config" description:"load config file(toml)" value-name:"CONFIG"`
+	Message string `short:"m" long:"message" description:"comment attached to the file" value-name:"MESSAGE"`
+	Channel string `short:"c" long:"channel" description:"channel in which your file will be sent" value-name:"CHANNEL"`
+	Title   string `short:"t" long:"title" description:"title attached to the file" value-name:"TITLE"`
+	Args    struct {
+		File string
+	} `positional-args:"yes" required:"yes"`
 }
 
 const (
@@ -22,7 +28,13 @@ const (
 )
 
 type Config struct {
-	SlackWebHook string
+	SlackToken string
+	Channels   []string
+}
+
+func Exists(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
 }
 
 func getConfigFilePath() (string, error) {
@@ -32,15 +44,17 @@ func getConfigFilePath() (string, error) {
 	}
 
 	dir := path.Join(usr.HomeDir, configDir)
-	err = os.Mkdir(dir, 0755)
-	if err != nil {
-		return "", err
+	if !Exists(dir) {
+		err = os.Mkdir(dir, 0755)
+		if err != nil {
+			return "", err
+		}
 	}
 	dst := path.Join(dir, configFile)
 	return dst, nil
 }
 
-//  指定されたtomlファイルを~/.nageru/config.toml にコピーする
+// LoadConfig は、指定されたtomlファイルを~/.nageru/config.toml にコピーする
 func LoadConfig(file string) error {
 	dst, err := getConfigFilePath()
 	if err != nil {
@@ -67,10 +81,23 @@ func LoadConfig(file string) error {
 	return nil
 }
 
+func ReadConfig() (*Config, error) {
+	src, err := getConfigFilePath()
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	_, err = toml.DecodeFile(src, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
 func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		panic(err)
+		os.Exit(-1)
 	}
 
 	if opts.Config != "" {
@@ -80,4 +107,37 @@ func main() {
 		}
 	}
 
+	config, err := ReadConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Configファイルの読み込みに失敗しました\n 理由: %#v", err)
+		os.Exit(-1)
+	}
+
+	file, err := os.Open(opts.Args.File)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ファイルの読み込みに失敗しました\n 理由: %#v", err)
+		os.Exit(-1)
+	}
+
+	var channels []string
+
+	if opts.Channel != "" {
+		channels = []string{opts.Channel}
+	} else {
+		channels = config.Channels
+	}
+
+	params := slack.FileUploadParameters{
+		Channels:       channels,
+		Filename:       path.Base(opts.Args.File),
+		InitialComment: opts.Message,
+		Reader:         file,
+		Title:          opts.Title,
+	}
+
+	api := slack.New(config.SlackToken)
+	if _, err := api.UploadFile(params); err != nil {
+		fmt.Fprintf(os.Stderr, "ファイルのアップロードに失敗しました\n 理由: %#v", err)
+		os.Exit(-1)
+	}
 }
